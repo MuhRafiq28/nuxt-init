6896de2ffb1a04fa0acf546e1603a21098d62c85
Rafiq <146798679+MuhRafiq28@users.noreply.github.com>
2024-08-02 15:00:02 +0700

    Update index.vue
    marapihkan

--- pages/index.vue
+++ pages/index.vue
@@ -1,19 +1,16 @@
 <template>
   <div class="w-full justify-center inline-flex">
     <div class="w-[1440px]">
-    <Navbar />
-    <Hero />
-      <Storya />
-      <Ottica />
-      <Visita />
-      <Footer />
+     <Navbar />
+     <Hero />
+     <Storya />
+     <Ottica />
+     <Visita />
+     <Footer />
     </div>
-    </div>
-
+  </div>
 </template>
 
 <style>
 @import url('https://fonts.googleapis.com/css2?family=Inter:wght@100..900&family=Playfair+Display:ital,wght@0,400..900;1,400..900&display=swap');
-
-
 </style>
